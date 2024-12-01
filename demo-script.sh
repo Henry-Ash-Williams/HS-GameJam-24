6e531e0b4eab19878ddcbca1ba9912b93b1eb3ef
Henry Williams <henrywilliams95000@gmail.com>
2024-12-01 12:56:33 +0000

    fucked it

--- demo-script.sh
+++ demo-script.sh
@@ -11,7 +11,7 @@ for i in $(seq 1 3); do
     alien_id=$(echo $alien | jq '.alien.alien_id' | sed 's/\"//g')
     image_url=$(curl -s "http://127.0.0.1:5000/generate-artwork/$alien_id" | jq -r '.image_url')
     alien_ids+=("$alien_id")  # Append the alien_id to the list
-    open $image_url
+    # open $image_url
 done
 echo 
 
